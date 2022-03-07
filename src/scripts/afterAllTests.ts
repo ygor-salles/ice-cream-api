9c33e886b1911642ac5fa1162eb4d35dff8a0ad5
unknown <ygorsallesaniceto@gmail.com>
2022-03-07 10:48:12 -0300

    refactor: setting afterAllTests enviroment variables

--- src/scripts/afterAllTests.ts
+++ src/scripts/afterAllTests.ts
@@ -3,13 +3,13 @@ import 'dotenv/config';
 
 async function dropDatabase() {
   const client = new Client({
-    connectionString: process.env.BD_URL_TEST,
+    connectionString: process.env.DATABASE_URL_TEST,
   });
 
   await client.connect();
   await client.query('DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC');
   await client.end();
-  return process.env.BD_TEST_DATABASE || '';
+  return process.env.BD_DATABASE_TEST || '';
 }
 
 dropDatabase()
